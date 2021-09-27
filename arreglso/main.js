3f5a95232642a6e90a22d3ab430dc15cdec51028
fernando vargas bautista <fernando_1320114093@uptecamac.edu.mx>
2021-09-27 14:29:19 -0500

    lista deberes

---
+++ arreglso/main.js
@@ -0,0 +1,106 @@
+// var elementos = [];
+// var colores = ["red", "green", "blue", "yellow"];
+
+// console.log(colores[2]);
+
+// var humano = {
+//     "informacion":{
+//         "nombre":"Fernando",
+//         "Edad": 19,
+//         "carrera": "ing Software"
+// },
+// "musica":{
+//     "generos":{
+//         "rock":{
+//         "canciones":{
+//             "uno":"Rock 1",
+//             "dos":"Rock 2",
+//             "tres":"Rock 3"
+//         },
+//         "albunes":{
+//             "uno":"Rock ALB 1",
+//             "dos":"Rock ALB 2",
+//             "tres":"Rock ALB 3"
+//         }
+//         },
+//         "electro":{
+//             "canciones":{
+//                 "uno":"Electro 1",
+//                 "dos":"Electro 2",
+//                 "tres":"Electro 3"
+//             }
+//         }
+//     }
+
+//   },
+//   "videojuegos":{
+//       "generos":{
+//           "shooters":{
+//               "shooters mas jugados":{
+//                   "uno":"star wars battlefront 2",
+//                   "dos":"battlefiel 1",
+//                   "tres":"halo 5"
+//               },
+//               "rol":{
+//                   "juegos de rol mas jugados":{
+//                       "uno":"the elder scroll",
+//                       "dos":"mass effect 2",
+//                       "tres":"south park the stick of truth"
+//                   }
+//               }
+//           }
+//       }
+//   },
+//   "comida":{
+//       "mexicana":{
+//           "uno":"tacos",
+//           "dos":"pozole",
+//           "tres":"tostadas de tinga"
+//       },
+//       "extranjera":{
+//           "uno":"pizza",
+//           "dos":"ramen",
+//           "tres":"hamburguesas"
+//       }
+//   },
+//   "series de anime":{
+//       "genero":{
+//           "accion":{
+//               "uno":"dragon ball",
+//               "dos":"caballeros del zodiaco",
+//               "tres":"black clover"
+//           },
+//           "terror":{
+//               "uno":"death note",
+//               "dos":"blood c",
+//               "tres":"Resident Evil: La tiniebla infinita"
+//           }
+//       }
+//     },
+//       "Peliculas":{
+//           "genero":{
+//               "accion":{
+//                   "uno":"transformer 3",
+//                   "dos":"avengers end games",
+//                   "tres":"deadpool"
+//               },
+//               "fantasia":{
+//                   "uno":"star wars",
+//                   "dos":"el señor de los anillos",
+//                   "tres":"godzilla vs kong"
+//               }
+//           }
+//     },
+// };
+// console.log(humano);
+
+function saludar(){
+    let pass = document.getElementById("jaja").value;
+    console.log(pass);
+    alert(pass);
+}
+
+function saludarConClase(){
+    let elemento = document.getElementsByClassName("jojo");
+    console.log(elemento);
+}
